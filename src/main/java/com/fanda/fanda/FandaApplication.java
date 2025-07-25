ed10f5a2a744e401888665c7dda44ea947b61a88
HyominAn0401 <slhm7547@naver.com>
2025-07-25 22:12:42 +0900

    initial commit

---
+++ src/main/java/com/fanda/fanda/FandaApplication.java
@@ -0,0 +1,13 @@
+package com.fanda.fanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fanda/fanda/FandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanda.fanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
