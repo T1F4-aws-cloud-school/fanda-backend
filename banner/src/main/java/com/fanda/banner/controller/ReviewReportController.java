01cd025ee8de3ec18dcd223cd957fc5d4e0523ef
HyominAn0401 <slhm7547@naver.com>
2025-08-07 16:33:36 +0900

    incomplete: authorize

--- banner/src/main/java/com/fanda/banner/controller/ReviewReportController.java
+++ banner/src/main/java/com/fanda/banner/controller/ReviewReportController.java
@@ -4,6 +4,7 @@ import com.fanda.banner.dto.ReportResponseDto;
 import com.fanda.banner.service.ReviewAnalysisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class ReviewReportController {
 
     private final ReviewAnalysisService reviewAnalysisService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/generate")
     public ResponseEntity<ReportResponseDto> generateReports(){
         return ResponseEntity.ok(reviewAnalysisService.generateAndUploadPdfReports());
