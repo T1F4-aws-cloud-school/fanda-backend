52385b7bced6cb14d7b39a67fc9e3bb8e6cd0ed1
Hyomin An <98948416+HyominAn0401@users.noreply.github.com>
2025-08-20 16:53:32 +0900

    feature: FND-333: etl client (#156)

--- recommend/src/test/java/com/fanda/recommend/RecommendApplicationTests.java
+++ recommend/src/test/java/com/fanda/recommend/RecommendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.fanda.user_behavior;
+package com.fanda.recommend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserBehaviorApplicationTests {
+class RecommendApplicationTests {
 
 	@Test
 	void contextLoads() {
