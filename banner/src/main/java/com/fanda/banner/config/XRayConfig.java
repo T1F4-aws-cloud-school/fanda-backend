bdc857df666b33fa98d6d78d217e3f81bf5868e8
jjeonge38 <131588594+jjeonge38@users.noreply.github.com>
2025-09-02 23:30:31 +0900

    fix/banner-xray (#278)
    Co-authored-by: jjeonge38 <kshjm151217@namer.com>

---
+++ banner/src/main/java/com/fanda/banner/config/XRayConfig.java
@@ -0,0 +1,19 @@
+package com.fanda.config;
+
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorderBuilder;
+import com.amazonaws.xray.plugins.EKSPlugin;
+import org.springframework.context.annotation.Configuration;
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class XRayConfig {
+
+    @PostConstruct
+    public void init() {
+        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard()
+                .withPlugin(new EKSPlugin());
+
+        AWSXRay.setGlobalRecorder(builder.build());
+    }
+}
